06000b849598ec9c67ec24ff8f80320c28d585d5
frucvill <fructuoso.villarroel@gmail.com>
2026-07-28 14:03:29 -0400

    fix in lazy loading in general views

--- frontendTodoVue/src/modules/todos/todo.routes.ts
+++ frontendTodoVue/src/modules/todos/todo.routes.ts
@@ -1,9 +1,20 @@
+// import type { RouteRecordRaw } from 'vue-router';
+
+// export const todoRoutes: RouteRecordRaw[] = [
+//   {
+//     path: '/todos',
+//     name: 'todos',
+//     component: () => import('./views/TodoView.vue'),
+//   },
+// ];
+
 import type { RouteRecordRaw } from 'vue-router';
+import TodoView from './views/TodoView.vue';
 
 export const todoRoutes: RouteRecordRaw[] = [
   {
     path: '/todos',
     name: 'todos',
-    component: () => import('./views/TodoView.vue'),
+    component: TodoView,
   },
 ];
